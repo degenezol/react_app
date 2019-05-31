0af9ef0cd0e97c7121359085d154bb4afd57377b
Oleg Lavrinenko <4oleg.lavrinenko@gmail.com>
2019-05-31 21:00:32 +0300

    create connect to gallery

--- src/components/pages/Gallery/index.js
+++ src/components/pages/Gallery/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addImageAction } from './actions';
 
 
-const Gallery = () => {
+const Gallery = ({ photos, addImage }) => {
+    const list = photos.map(({ id, name }) => <li key={id}>{name}</li>);
+    const add = () => {
+        const id = photos[photos.length - 1]['id'] + 1;
+        const image = {id, name: `image-${id}`};
+        addImage(image);
+    }
     return (
         <div id='gallery' className='page'>
             <div className='page-wrap'>
                 <h1>Gallery</h1>
+                <ul>
+                    {list}
+                </ul>
+                <input type="button" onClick={add} value="add image" />
             </div>
         </div>
     )
 }
 
-export default Gallery;
+const mapStateToProps = ({ gallery: { photos } }) => ({
+    photos: photos
+})
+
+const mapDispatchToProps = {
+    addImage: addImageAction,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Gallery);
